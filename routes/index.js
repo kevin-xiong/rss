add8fe7f2610789869e571381fda555d76b26b3d
xwj <xiongwenjun944@lu.com>
2017-05-27 02:56:45 +0800

    添加源功能

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,23 @@
 var express = require('express');
 var router = express.Router();
 var fetch = require('node-fetch');
-FeedParser = require('feedparser'),
+var FeedParser = require('feedparser');
 /* GET home page. */
+const low = require('lowdb')
+var db = low('./config/db.json');
+db.defaults({ urls: [] }).write()
 router.get('/', function(req, res, next) {
   var spide = require('rssspider');
-var url = 'https://www.matrix67.com/blog/feed';
-spide.fetchRss(url).then(function(data){
-		res.render('index', { title: 'Express',b:data });
-});
+  var url = 'https://www.matrix67.com/blog/feed';
+   
+  var num = db.get('urls').find({ name: url }).size().value()
+  if(num == 0){
+    db.get('urls').push({ name: url }).write()
+  }
+  spide.fetchRss(url).then(function(data){
+        res.render('index', { title: 'Rss',b:data });
+    });
+  
   
 });
 
